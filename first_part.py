27373a60c7be231266ee1cf34c5463938b00de2d
Eduardo Martinez Palomino <eduardomp89@MacBook-Air-de-Eduardo.local>
2022-07-20 13:06:05 +0200

    Version de victor en coversor de entero a romanos y primera version de romanos a entero

--- first_part.py
+++ first_part.py
@@ -1,8 +1,6 @@
 """
 1. Crear una función que pase de entero > 0 y < 4000 a romano
 2. Cualquier otra entrada debe dar error
-
-
 """
 
 class RomanNumberError(Exception):
@@ -12,62 +10,57 @@ numero_romano = (
     (1000, 'M'), (500, 'D'), (100, 'C'), (50, 'D'), (10, 'X'), (5, 'V'), (1, 'I')
 )
 
-componentes = (
-    ( # millares
-        (1000, 'M'), (2000, 'MM'), (3000, 'MMM')
-    ), 
-    ( # centenas
-        (100, 'C'), (200, 'CC'), (300, 'CCC'),
+#Versión Victor:
+millares = ((1000, 'M'), (2000, 'MM'), (3000, 'MMM')) 
+
+centenas = ((100, 'C'), (200, 'CC'), (300, 'CCC'),
         (400, 'CD'), (500, 'D'), (600, 'DC'),
-        (700, 'DCC'), (800, 'DCCC'), (900, 'CM')
-    ),
-    ( # decenas
-        (10, 'X'), (20, 'XX'), (30, 'XXX'),
+        (700, 'DCC'), (800, 'DCCC'), (900, 'CM'))
+
+decenas = ((10, 'X'), (20, 'XX'), (30, 'XXX'),
         (40, 'XL'), (50, 'L'), (60, 'LX'),
-        (70, 'LXX'), (80, 'LXXX'), (90, 'XC')
-    ),
-    ( # unidades
-        (1, 'I'), (2, 'II'), (3, 'III'),
+        (70, 'LXX'), (80, 'LXXX'), (90, 'XC'))
+    
+unidades = ((1, 'I'), (2, 'II'), (3, 'III'),
         (4, 'IL'), (5, 'V'), (6, 'VI'),
-        (7, 'VII'), (8, 'VIII'), (9, 'IX')
-    )
-
-)
-
+        (7, 'VII'), (8, 'VIII'), (9, 'IX'))
 
 def entero_a_romano(numero):
-    """
-    numero = str(numero)
-    longitud = len(numero)
 
-    if longitud < 4:
-        numero = "{:0>4s}".format(numero)
-    """
     numero = "{:0>4d}".format(numero)
     digitos = list(numero)
 
     ix = 0
-    longitud = len(digitos)
+    longitud = 4
+
     for n in numero:
         longitud -= 1
         digitos[ix] = digitos[ix] + "0" * longitud
         ix += 1
 
-    """
-    digitos = ['0000', '300', '30', '6']
-    """
+    traduccion = []
 
-# procesamos millares
-    traduccion = ""
-    ix = 0
-    for componente in componentes:
-        for cifra, simbolo in componente:
-            if str(cifra) == digitos[ix]:
-                traduccion += simbolo
-                break
-        ix += 1
+    for m in millares:
+        if digitos [0] == str(m[0]):
+            traduccion.append(m[1])
+            break
 
-    return traduccion
+    for c in centenas:
+        if digitos [1] == str (c[0]):
+            traduccion.append(c[1])
+            break
 
+    for d in decenas:
+        if digitos [2] == str (d[0]):
+            traduccion.append(d[1])
+            break
+    
+    for u in unidades:
+        if digitos [3] == str (u[0]):
+            traduccion.append(u[1])
+            break
 
-entero_a_romano(336)
+    return "".join(traduccion)
+
+valor = int(input("Introduce un número entero y te lo devolverá en Romano: "))
+print(entero_a_romano(valor))
---
+++ two_part.py
@@ -0,0 +1,72 @@
+componentes = {
+    1000: 'M', 2000: 'MM', 3000: 'MMM', 
+    100: 'C', 200: 'CC', 300: 'CCC',
+    400: 'CD', 500: 'D', 600: 'DC',
+    700: 'DCC', 800: 'DCCC', 900: 'CM',
+    10: 'X', 20: 'XX', 30: 'XXX',
+    40: 'XL', 50: 'L', 60: 'LX',
+    70: 'LXX', 80: 'LXXX', 90: 'XC',
+    1: 'I', 2: 'II', 3: 'III',
+    4: 'IL', 5: 'V', 6: 'VI',
+    7: 'VII', 8: 'VIII', 9: 'IX'
+}
+
+
+def convertir_dic_romano():
+    dic_romano = {}
+    for clave,valor in componentes.items():
+        dic_romano[valor] = clave
+    return dic_romano
+
+def romano_a_entero(n_romano):
+    error = ""
+    dic_romano = convertir_dic_romano() 
+
+    n_romano2 = n_romano
+    numero_entero = 0
+    posicion_n_romano = 0
+    while posicion_n_romano<len(n_romano):
+
+        n =  n_romano[posicion_n_romano]
+        valor_entero = dic_romano.get(n,"error") 
+        if valor_entero == "error":
+            numero_entero = 0
+            error = "Valores Incorrectos"
+            break 
+
+        ix=posicion_n_romano + 1
+
+        contador_posiciones_sumar = 0
+        contador_valores_iguales  = 1
+        while ix<len(n_romano2):
+            valor_siguiente = n_romano[ix]
+            valor_entero_siguiente = dic_romano.get(valor_siguiente,"error") 
+            if valor_entero_siguiente == "error":
+                error = "Valores Incorrectos"
+                break
+            elif valor_entero_siguiente > valor_entero:
+                valor_entero = valor_entero_siguiente - valor_entero
+                contador_posiciones_sumar += 1 
+            elif valor_entero_siguiente == valor_entero:
+                contador_valores_iguales += 1
+            else:
+                break
+            ix += 1 
+
+        if contador_valores_iguales > 3:
+            numero_entero = 0
+            error = "El número romano no es correcto"
+            break
+
+        numero_entero += valor_entero
+        posicion_n_romano += 1 + contador_posiciones_sumar
+
+    return numero_entero,error
+
+
+valor = input("Número romano:")
+valor_romano,error = romano_a_entero(valor)
+if error == "":
+    print("Valor entero: ",  valor_romano)
+else:
+    print(error)
